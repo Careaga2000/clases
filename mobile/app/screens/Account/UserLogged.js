0644881743e47609e0ff57dc617858efad76aabe
Careaga2000 <careaga.snow@gmail.com>
2022-03-30 20:21:03 -0600

    Creacion del login

--- mobile/app/screens/Account/UserLogged.js
+++ mobile/app/screens/Account/UserLogged.js
@@ -3,8 +3,8 @@ import {View, Text} from 'react-native'
 
 export default function UserLogged(){
     return(
-        <view>
-            <text> UserLogged  </text>
-        </view>
+        <View>
+            <Text> UserLogged  </Text>
+        </View>
     )
 }
